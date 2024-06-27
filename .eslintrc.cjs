d8f5462239a6ca3672e318256ba88c421464ccf2
Brian Schlenker <bschlenk@umich.edu>
2024-06-27 00:32:13 -0400

    set up simple import sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,13 +8,38 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Side effect imports.
+          ['^\\u0000'],
+          // Node.js builtins prefixed with `node:`.
+          ['^node:'],
+          // Packages (react first).
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^react$', '^react-dom$', '^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+          // Css imports
+          ['\\.css$', '\\.module\\.css$'],
+        ],
+      },
+    ],
   },
 }
