ea3c9f85310cc1d374bfd52b145643befb1da54b
Brian Schlenker <bschlenk@umich.edu>
2024-06-27 23:08:48 -0400

    fix svg wrong spelling

--- src/components/icon-rotate.tsx
+++ src/components/icon-rotate.tsx
@@ -4,8 +4,8 @@ export function IconRotate() {
       <path
         d="M18.0994 11.5643C18.0994 8.1957 15.3686 5.4649 12 5.4649C8.63139 5.4649 5.9006 8.1957 5.9006 11.5643C5.9006 14.9329 8.63139 17.6637 12 17.6637C14.1074 17.6637 15.9104 16.6747 17.0061 15.0497M17.0061 18.5351V15.0497M17.0061 15.0497H13.5208"
         stroke="var(--color-icon)"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
--- src/components/icon-scale.tsx
+++ src/components/icon-scale.tsx
@@ -4,8 +4,8 @@ export function IconScale() {
       <path
         d="M6.5 6.5L17.5 17.5M6.5 6.5L10.5 6.5M6.5 6.5V10.5M17.5 17.5L17.5 13.5M17.5 17.5H13.5"
         stroke="var(--color-icon)"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
